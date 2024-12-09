0285d4b0b031b13232b300429c3b576c842114c6
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-12-09 14:06:00 +0100

    express inti

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import { commandSurvey } from './actions/comand-survey.js';
 import { app } from './lib/slack-app.js';
 import { scheduledQuestionsToUsers } from './utils/questions/scheduled-question-to-users.js';
+import  express from 'express';
 
+const appExpress = express();
+appExpress.listen(process.env.PORT || 3000 ,()=> {console.log(process.env.PORT || 3000);})
 commandSurvey();
 scheduledQuestionsToUsers();
 
