4d86c29d261c12361ab77227e89573aa390f439c
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-12-16 17:51:07 +0100

    rename Loïse Rocheteau

--- shared/constants.js
+++ shared/constants.js
@@ -1,2 +1,2 @@
 export const RANGE_GOOGLE_SHEET = 'A1:F1';
-export const usersTeamProduit = process.env.NODE_ENV === 'development' ? ['Henri-Pierre Rigoulet'] : ['Louise Rocheteau', 'Bruno Griveau', 'François Pagnon', 'Diogo De Araujo', 'Charles Goddet', 'Stan Husson'];
+export const usersTeamProduit = process.env.NODE_ENV === 'development' ? ['Henri-Pierre Rigoulet'] : ['Loïse Rocheteau', 'Bruno Griveau', 'François Pagnon', 'Diogo De Araujo', 'Charles Goddet', 'Stan Husson'];
