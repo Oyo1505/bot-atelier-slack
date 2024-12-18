caa282bedef845e9694bba1a9de7b7f87ebaca5c
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2024-12-18 11:07:14 +0100

    fixed bug

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 import { commandSurvey } from './actions/comand_survey.js';
 import { commandRapport } from './actions/command_rapport.js';
 import { app } from './lib/slack-app.js';
-import { scheduledQuestionsToUsers } from './utils/questions/scheduled-question-to-users.js';
+import { scheduledQuestionsToUsersEachThursday } from './utils/questions/scheduled-question-to-users.js';
 import  express from 'express';
 import { scheduledQuestions } from './utils/questions/scheduled-questions.js';
+import { isAuthorizedUser } from './utils/bool/is-authorized-user.js';
 
 const appExpress = express();
 
 appExpress.listen(process.env.PORT || 3000 ,()=> {console.log(process.env.PORT || 3000);})
+app.command('/test', async ({ ack, body }) => {
+  if(!isAuthorizedUser(body.user_id)) return
+  try {
+   await ack();
+   app.client.chat.postMessage({
+    channel: body.user_id,
+    text: 'test',
+  });
+  } catch (error) {
+    console.log(error);
+  }
+})
 commandRapport();
 commandSurvey();
-scheduledQuestionsToUsers();
+scheduledQuestionsToUsersEachThursday();
 scheduledQuestions();
+
 
 (async () => {
   await app.start();
-  console.log('⚡️ Bolt app is running!', process.env.PORT || 7000 );
+  console.log('⚡️ Bolt app is running!', process.env.PORT || 3000 );
 })();
--- utils/questions/scheduled-question-to-users.js
+++ utils/questions/scheduled-question-to-users.js
@@ -12,7 +12,7 @@ const DAYS_OF_WEEK = 4; //Jeudi
 const SCHEDULE_TIME = `${SECONDES} ${MINUTES} ${HOURS} ${DAYS_OF_MONTH} ${MONTHS} ${DAYS_OF_WEEK}`;
 
 
-export const scheduledQuestionsToUsers = async () => {
+export const scheduledQuestionsToUsersEachThursday = async () => {
  //await deleteAllFiles()
   cron.schedule(SCHEDULE_TIME, async () => {
     await sendQuestionsToUsers();
